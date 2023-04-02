db70f8366081a82b9d20358de836c57580263521
Luis Pereira <luispereira.tkd@gmail.com>
2023-04-02 23:21:50 +0100

    completed implementation of mockup onto vue app

--- src/components/card/ButterflyCard.vue
+++ src/components/card/ButterflyCard.vue
@@ -1,24 +1,40 @@
 <script setup lang="ts">
 import type { Butterfly } from '@/types/butterfly';
+import { computed } from 'vue';
 
-defineProps<{
-    butterfly: Butterfly,
-    index: number
-    expanded: boolean
-    }>()
+const props = defineProps<{
+    butterfly: Butterfly;
+    index: number;
+    expanded: boolean;
+}>();
 
+const butterflyImageUrl = computed(() => {
+    return `url(${props.butterfly.image})`;
+});
+
+const butterflyCardPosition = computed(() => {
+    if (props.expanded) {
+        return {};
+    }
+    return {
+        'z-index': props.index + 10,
+        left: `${10 + 30 * props.index}px`
+    };
+});
 </script>
 
 <template>
-    <article class="card">
-        <h3 v-if="!expanded" class="card-text__side">{{butterfly.scientific}}</h3>
-        <div class="card-image background"></div>
+    <article class="card" :style="butterflyCardPosition">
+        <h3 v-if="!expanded" class="card-text__side">{{ butterfly.scientific }}</h3>
+        <div class="card-image background" :style="{ 'background-image': butterflyImageUrl }"></div>
         <div class="card-text">
-            <h3>{{butterfly.scientific}}</h3>
-            <h5><em>({{butterfly.described}})</em></h5>
+            <h3>{{ butterfly.scientific }}</h3>
+            <h5>
+                <em>({{ butterfly.described }})</em>
+            </h5>
             <p v-if="butterfly.details && butterfly.details.length > 0">
-                {{butterfly.details[0].description.slice(0, 200)}}
-                {{butterfly.details[0].description.length > 200 ? '...' : ''}}
+                {{ butterfly.details[0].description.slice(0, 200) }}
+                {{ butterfly.details[0].description.length > 200 ? '...' : '' }}
             </p>
         </div>
         <div class="card-stats">
@@ -26,11 +42,11 @@ defineProps<{
                 <div class="value" title="Noturna">&#127769;</div>
             </div>
             <div class="stat">
-                <div class="value">{{butterfly.appearances}}</div>
+                <div class="value">{{ butterfly.appearances }}</div>
                 <div class="type">aparições</div>
             </div>
             <div class="stat">
-                <div class="value">{{butterfly.size}}</div>
+                <div class="value">{{ butterfly.size }}</div>
                 <!--<div class="type">envergadura</div>-->
             </div>
         </div>
@@ -39,120 +55,117 @@ defineProps<{
 
 <style scoped>
 .card:hover {
-
-  /*   animation: pick 3s ease-in-out;*/
-  box-shadow: 5px 5px 15px rgba(0,0,0,0.6);
+    /*   animation: pick 3s ease-in-out;*/
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.6);
 }
 
 .card {
-  display: flex;
-  /*transform: rotate3d(-1, -1, 0, 15deg);*/
-  flex-direction: column;
-  cursor: pointer;
+    display: flex;
+    /*transform: rotate3d(-1, -1, 0, 15deg);*/
+    flex-direction: column;
+    cursor: pointer;
 
-  border-radius: 18px;
-  width: 300px;
-  min-width: 300px;
-  height: 500px;
-  background: white;
-  box-shadow: 5px 5px 15px rgba(0,0,0,0.9);
-  font-family: roboto;
-  text-align: center;
+    border-radius: 18px;
+    width: 300px;
+    min-width: 300px;
+    height: 500px;
+    background: white;
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.9);
+    font-family: roboto;
+    text-align: center;
 }
 
 .card-image {
-  height: 210px;
-  overflow: hidden;
+    height: 210px;
+    overflow: hidden;
 }
 .card-text {
-  height: 210px;
+    height: 210px;
 }
 .card-stats {
-  height: 80px;
+    height: 80px;
 }
 
-.card-image.background{
-  background-image: url("https://www.monaconatureencyclopedia.com/wp-content/uploads/2022/01/1-Lymantria-dispar-%C2%A9-Jason-McCombe.jpg" );
-  background-position: center;
-  border-top-left-radius: 15px;
-  border-top-right-radius: 15px;
-  background-size: cover;
+.card-image.background {
+    background-position: center;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    background-size: cover;
 }
 
 .card-image img {
-  max-width: 100%;
-  /*height: auto;*/
-  border-top-left-radius: 15px;
-  border-top-right-radius: 15px;
-  object-fit: cover;
-  /*object-position: 80% 50%;*/
+    max-width: 100%;
+    /*height: auto;*/
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    object-fit: cover;
+    /*object-position: 80% 50%;*/
 }
 
 .card-text {
-  padding: 15px 25px;
+    padding: 15px 25px;
 }
 
 .card-text p {
-  color: grey;
-  font-size:15px;
-  font-weight: 300;
+    color: grey;
+    font-size: 15px;
+    font-weight: 300;
 }
 
 .card-text h3 {
-  margin:0px;
-  font-size:18px;
+    margin: 0px;
+    font-size: 18px;
 }
 
 .card-text h5 {
-  margin-top:0px;
-  color: grey;
+    margin-top: 0px;
+    color: grey;
 }
 
-.card-stats { 
-  display: flex;
-  width: 100%;
-  justify-content: space-around;
-  align-items: center;
-  border-bottom-left-radius: 15px;
-  border-bottom-right-radius: 15px;
-  background: rgb(22 163 74);
+.card-stats {
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    align-items: center;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    background: rgb(22 163 74);
 }
 
 .card-stats .stat {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 
-  color: white;
-  padding: 10px;
+    color: white;
+    padding: 10px;
 }
 
 .card-stats .stat.daytime {
-  font-size:24px;
+    font-size: 24px;
 }
 
 .card:hover {
-  z-index: 100;
+    z-index: 100 !important;
 }
 
 .card-text__side {
-  position: absolute;
-  background-color: rgba(255,255,255, 0.3);
-  border-top-left-radius: 15px;
-  padding: 2px;
-  height: 210px;
-  width: 50px;
-  /* left: -20px; */
-  left: 0px;
-  writing-mode: sideways-lr;
-  text-orientation: mixed;
-  color: #4d4d4d;
-  z-index: 100;
+    position: absolute;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-top-left-radius: 15px;
+    padding: 2px;
+    height: 210px;
+    width: 50px;
+    /* left: -20px; */
+    left: 0px;
+    writing-mode: sideways-lr;
+    text-orientation: mixed;
+    color: #4d4d4d;
+    z-index: 100;
 }
 
 .card:hover .card-text__side {
-  display: none;
+    display: none;
 }
-
 </style>
--- src/views/MonitoringView.vue
+++ src/views/MonitoringView.vue
@@ -15,59 +15,49 @@
         </div>
     </div>
     <h3 class="my-2 font-bold text-lg">Monitoring Butterflies</h3>
-    <ul v-if="state.monitoring">
-        <li v-for="appearance in state.monitoring.appearances" :key="appearance.id">
-            <div>
-                <label class="font-bold">Name: </label>
-                <span>{{butterflyIdentification(appearance)}}
-                    <em class="text-gray-400 ml-2" v-if="appearance.butterfly.details">
-                        {{appearance.butterfly.details[0].common_name}}
-                    </em>
-                </span>
-            </div>
-        </li>
-    </ul>
-    <div class="relative flex" v-if="state.monitoring && state.monitoring.appearances">
-        <ButterflyCard 
+    <div class="relative flex butterfly-exhibit" v-if="state.monitoring && state.monitoring.appearances">
+        <ButterflyCard
             :key="appearance.id"
-            v-for="(appearance, index) in state.monitoring.appearances" 
-            :butterfly="appearance.butterfly" 
+            v-for="(appearance, index) in state.monitoring.appearances"
+            :butterfly="appearance.butterfly"
             :index="index"
-            :expanded="true"
-            />
+            :expanded="state.monitoring.appearances.length == 1"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-    import { onMounted, reactive } from 'vue'
-    import makeRequest from '../utils/makeRequest'
-    import type { Monitoring, ButterflyAppearance } from '../types/monitoring'
-    import type { ApiResponse } from '../types/common'
-    import ButterflyCard from '../components/card/ButterflyCard.vue'
+import { onMounted, reactive } from 'vue';
+import makeRequest from '../utils/makeRequest';
+import type { Monitoring } from '../types/monitoring';
+import type { ApiResponse } from '../types/common';
+import ButterflyCard from '../components/card/ButterflyCard.vue';
 
-    interface MonitoringViewState {
-        monitoring: null | Monitoring
+interface MonitoringViewState {
+    monitoring: null | Monitoring;
+}
+
+const state: MonitoringViewState = reactive({
+    monitoring: null
+});
+
+onMounted(async () => {
+    const response = await makeRequest<undefined, ApiResponse<Monitoring>>('monitoring/1', 'get');
+    if (response.status === 200) {
+        const data = response.json as ApiResponse<Monitoring>;
+        console.log(data.records);
+        state.monitoring = data.records;
     }
-
-    const state: MonitoringViewState = reactive({
-        monitoring: null
-    })
-
-    const butterflyIdentification = (monitoringAppearance: ButterflyAppearance):string  => {
-        if (!monitoringAppearance) {
-            return '';
-        }
-
-        const identification = monitoringAppearance.butterfly.scientific || 'unknown name';
-        return `${identification} - (${monitoringAppearance.butterfly.described})`
-    }
-
-    onMounted(async() => {
-        const response = await makeRequest<undefined, ApiResponse< Monitoring>>('monitoring/1', 'get')
-        if (response.status === 200) {
-            const data = response.json as ApiResponse<Monitoring>
-            console.log(data.records)
-            state.monitoring = data.records
-        }
-    })
+});
 </script>
+
+<style scoped>
+.butterfly-exhibit {
+    overflow: scroll;
+    height: 600px;
+}
+
+.butterfly-exhibit .card {
+    position: absolute;
+}
+</style>
