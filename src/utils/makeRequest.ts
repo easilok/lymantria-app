75911c383d9bf59fe77f5ef2553b34888b5add25
Luis Pereira <luispereira.tkd@gmail.com>
2023-04-29 19:18:56 +0100

    added env variable to set api url

--- src/utils/makeRequest.ts
+++ src/utils/makeRequest.ts
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:8080/api/';
-const API_VERSION = '';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/';
+const API_VERSION = import.meta.env.VITE_API_VERSION || '';
 
 export interface FetchResponse<R> {
     status: number;
